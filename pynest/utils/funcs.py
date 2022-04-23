1b6128880da6d9e146642bd10be10f35b0ebf122
Gi <marinelli.giovanna@gmail.com>
2022-04-23 20:09:12 +0000

    removed border

--- pynest/utils/funcs.py
+++ pynest/utils/funcs.py
@@ -17,12 +17,7 @@ def convex_hull_polygon(points):
 
     return chull
 
-def create_rectangle(xmin, xmax, ymin, ymax, expand_border:float = 2.5) -> Polygon:
-    xmin -= expand_border
-    xmax += expand_border
-    ymin -= expand_border
-    ymax += expand_border
-    
+def create_rectangle(xmin, xmax, ymin, ymax) -> Polygon:
     s1 = Segment(xmin, ymin, xmin, ymax)
     s2 = Segment(xmin, ymax, xmax, ymax)
     s3 = Segment(xmax, ymax, xmax, ymin)

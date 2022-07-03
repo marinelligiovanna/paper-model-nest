f61e3f221fb454cb035ba3e5024792d73da7764d
Gi <marinelli.giovanna@gmail.com>
2022-07-03 21:09:50 +0000

    feat: method to save bin

--- pynest/packing/bin.py
+++ pynest/packing/bin.py
@@ -29,7 +29,14 @@ class Bin:
         plt.figure(figsize=(9,6))
         
         for rect in self.rects:
-            rect.plot(color="r", show_names=show_names, show_rects=show_rects)
+            rect.plot(color="r", show_names=show_names, show_rect=show_rects)
 
         plt.xlim(0, self.width)
         plt.ylim(0, self.height)
+
+    def save(self, fname:str, format:str="svg", **kwargs) -> None:
+        self.plot(**kwargs)
+        name = f"{fname}.{format}"
+        plt.axis('off')
+        plt.savefig(name, format=format, bbox_inches='tight', pad_inches=0)
+        plt.close(name)
